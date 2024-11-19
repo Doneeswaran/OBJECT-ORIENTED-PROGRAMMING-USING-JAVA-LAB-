8e678ea79015dad01518e3d442514a26597948eb
Doneeswaran <231501502@rajalakshmi.edu.in>
2024-11-19 19:19:19 +0530

    Add files via upload

---
+++ JAVA PROJECT/login and sign/src/login/and/sign/LoginAndSign.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package login.and.sign;
+
+public class LoginAndSign {
+
+    public static void main(String[] args) {
+        login_page l1 = new login_page();
+        l1.setVisible(true);
+    }
+    
+}
